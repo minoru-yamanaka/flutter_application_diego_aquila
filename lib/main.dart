acfcc2ab2294eebe28c6244e380fa8d800b21919
paulo.myamanaka <minoruyamanaka@icloud.com>
2025-09-16 20:29:25 -0300

    alteração de app.dart e main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,6 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_application_diego_aquila/app.dart';
-
-// void main() {
-//   runApp(const MyApp());
-// }
-
 import 'package:flutter/material.dart';
-import 'package:flutter_application_diego_aquila/pages/login_page.dart'; // Importe a sua nova tela de login
+import 'package:flutter_application_diego_aquila/app.dart'; // Importa o widget principal
 
 void main() {
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Diego Áquila App',
-      debugShowCheckedModeBanner: false, // Remove a faixa de "Debug"
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFFF58524)),
-        useMaterial3: true,
-        fontFamily: 'Roboto', // Você pode escolher outra fonte se preferir
-      ),
-      home: const LoginPage(), // A tela inicial agora é a de login
-    );
-  }
 }
